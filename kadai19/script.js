4a5cae3ce99d38b3317ed74704d299e9c22f23c5
c3240659gku <c3240659@std.gku.ac.jp>
2025-12-16 11:49:37 +0900

    ok

---
+++ kadai19/script.js
@@ -0,0 +1,29 @@
+function calc() {
+    let n = prompt("範囲の最大値を1～10で入力してください");
+    n = Number(n);
+    if(!Number.isInteger(n) || n < 1 || n > 10) {
+        alert("1~10の整数を入力してください");
+        return;
+    }
+
+    function gcd(a,b) {
+        while (b !== 0) {
+            let temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+
+    function lcm(a,b) {
+        return (a * b) / gcd(a,b);
+    }
+
+    let answer = 1;
+    for(let i = 1; i <= n; i++){
+        answer = lcm(answer,i);
+    }
+
+    document.getElementById("result").textContent =
+        '1~${n}で割り切れる最小の数値は${answer}です。';
+}
